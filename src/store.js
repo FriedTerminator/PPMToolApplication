600497b3b4c0ae826ac6352d55c1ac61decb11ad
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2024-09-25 16:06:17 -0700

    project broke, now fixed

--- src/store.js
+++ src/store.js
@@ -2,27 +2,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initalState = {};
+const initialState = {};
 const middleware = [thunk];
 
-let store;
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-if (window.navigator.userAgent.includes("Chrome")) {
-  store = createStore(
-    rootReducer,
-    initalState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  store = createStore(
-    rootReducer,
-    initalState,
-    compose(applyMiddleware(...middleware))
-  );
-}
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 export default store;
