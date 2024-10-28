773561e1e397f117c4a24dc794c75189e62420e9
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2024-10-28 15:44:17 -0700

    created delete option for projects

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECT, GET_PROJECTS } from "../actions/types";
+import { GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "../actions/types";
 
 const initialState = {
     projects: [], 
@@ -10,12 +10,17 @@ export default function(state = initialState, action) {
         case GET_PROJECTS:
             return {
                 ...state,
-                projects:action.payload
+                projects: action.payload
             };
         case GET_PROJECT:
             return {
                 ...state,
                 project: action.payload
+            };
+        case DELETE_PROJECT:
+            return {
+                ...state,
+                projects: state.projects.filter(project => project.projectIdentifier !== action.payload)
             };
         default:
             return state;
