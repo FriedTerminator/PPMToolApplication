bce3902eba89dc0dfd0530dcc2f3addadef69ffe
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-01-27 13:20:38 -0800

    created securityReducer and securityActions

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import { jwtDecode } from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
     try {
@@ -9,6 +11,34 @@ export const createNewUser = (newUser, history) => async dispatch => {
             type: GET_ERRORS,
             payload: {}
         })
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        });
+    }
+};
+
+export const login = LoginRequest => async dispatch => {
+    try {
+        // post => Login Request
+        const res = await axios.post("/api/users/login", LoginRequest)
+
+        // extract token from res.data
+        const {token} = res.data;
+
+        // store the token in the local storage
+        localStorage.setItem("jwtToken", token);
+
+        // set our token in the header ***
+        setJWTToken(token);
+        // decode token on React
+        const decoded = jwtDecode(token);
+        // dispatch to our securityReducer
+        dispatch({
+            type: SET_CURRENT_USER,
+            payload: decoded
+        });
     } catch (error) {
         dispatch({
             type: GET_ERRORS,
