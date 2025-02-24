ded5baf4fc0f88b05e7c18a7758a546bf205a605
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-02-24 12:37:13 -0800

    tiny refactor

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -42,7 +42,7 @@ class Header extends Component {
 
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/Register">
+            <Link className="nav-link" to="/register">
               Sign Up
             </Link>
           </li>
